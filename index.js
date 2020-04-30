81c356a76b10199e1fd614fe5a4bcfb66d99b6fd
Bakugou808 <janu.m.sung@gmail.com>
2020-04-30 14:24:59 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
-// Your code here
+const reduceToTotal = (sourceArray, startingPoint=0) => {
+    let memo = startingPoint
+    for(let i = 0; i < sourceArray.length; i++){
+        memo += sourceArray[i]
+    }
+    return memo
+}
+
+
+function reduceToAllTrue(sourceArray){
+    let memo = true
+    for(let i = 0; i < sourceArray.length; i++){
+        if (sourceArray[i] === false){
+            memo = false 
+            return memo 
+        }
+    }
+    return memo
+}
+
+
+const reduceToAnyTrue = (sourceArray)=>{
+    let memo = true
+    for(let i = 0; i < sourceArray.length; i++){
+        if (sourceArray[i] === true){
+            return memo 
+        }
+    }
+    memo = false
+    return memo
+}
+
+
+
+
+
+
+
+
+function mapToNegativize(sourceArray){
+    let nuArr = []
+    for(let i = 0; i < sourceArray.length; i++){
+        nuArr.push(sourceArray[i] * -1)
+    }
+    return nuArr 
+}
+
+
+const mapToNoChange = (sourceArray) => {
+    let nuArr = []
+    for(let i = 0; i < sourceArray.length; i++){
+        nuArr.push(sourceArray[i])
+    }
+    return nuArr
+}
+
+
+function mapToDouble(sourceArray){
+    let nuArr = []
+    for(let i = 0; i < sourceArray.length; i++){
+        nuArr.push(sourceArray[i] * 2)
+    }
+    return nuArr
+}
+
+
+const mapToSquare = (sourceArray)=>{
+    let nuArr = []
+    for(let i = 0; i < sourceArray.length; i++){
+        nuArr.push(sourceArray[i] ** 2)
+    }
+    return nuArr
+}
