649ea37094430735e55f7512aaca4c24a58c08fa
github username <github email address>
2020-04-30 17:48:25 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    let array = []
+    for (let i = 0; i < sourceArray.length; i++ ) {
+      array.push(-1 * sourceArray[i])
+    }
+    return array
+}
+
+function mapToNoChange(sourceArray) {
+    let array = []
+    for (let i = 0; i < sourceArray.length; i++ ) {
+      array.push(sourceArray[i])
+    }
+    return array
+}
+
+function mapToDouble(sourceArray) {
+    let array = []
+    for (let i = 0; i < sourceArray.length; i++ ) {
+      array.push(2* sourceArray[i])
+    }
+    return array
+}
+
+function mapToSquare(sourceArray) {
+    let array = []
+    for (let i = 0; i < sourceArray.length; i++ ) {
+      array.push(sourceArray[i]*sourceArray[i])
+    }
+    return array
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+    let total=startingPoint
+
+    for (let i = 0; i < sourceArray.length; i++ ) {
+      total = total + sourceArray[i]
+    }
+    return total
+}
+
+function reduceToAllTrue(sourceArray) {
+    let check=true
+
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        if (!sourceArray[i]) {
+      check = !!sourceArray[i]
+        }
+    }
+    return check
+
+}
+
+function reduceToAnyTrue(sourceArray) {
+    let check=false
+
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        if (sourceArray[i]) {
+      check = !!sourceArray[i]
+        }
+    }
+    return check
+
+}
